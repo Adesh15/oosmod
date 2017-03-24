adbc4868a344847b451ea6c46e482367d84ec81d
adesh15 <adesikha15@gmail.com>
2017-03-24 06:04:43 +0100

    Revert "mm: fix process reclaim"
    This reverts commit 109f4ef773058f8affa43e23d072ead516713dae.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -11,8 +11,6 @@
 #include <linux/rmap.h>
 #include <linux/swap.h>
 #include <linux/swapops.h>
-#include <linux/mm_inline.h>
-#include <linux/ctype.h>
 
 #include <asm/elf.h>
 #include <asm/uaccess.h>
